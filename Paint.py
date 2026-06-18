c140f5f27624dfa65662c1f9cf749787e67ff2e2
ij-eng <ianjonasnascimento4@gmail.com>
2026-06-18 10:51:04 -0300

    Add files via upload

---
+++ Paint.py
@@ -0,0 +1,92 @@
+from tkinter import *
+from tkinter import ttk
+
+def iniciar_figura_nova(event): 
+    global figura_nova
+    if tipo_figura_var.get() == 'Linha':
+        figura_nova = ("linha", (event.x, event.y, event.x, event.y))
+    elif tipo_figura_var.get() == 'rabisco':
+        figura_nova = ("rabisco", [(event.x, event.y)])
+    elif tipo_figura_var.get() == 'retangulo':
+        figura_nova = ('retangulo',(event.x, event.y, event.x, event.y))
+    elif tipo_figura_var.get() == 'circulo':
+        figura_nova = ('circulo',(event.x, event.y, event.x, event.y))
+
+def atualizar_figura_nova(event):
+    global figura_nova
+    if figura_nova[0] == "rabisco":
+        figura_nova[1].append([(event.x, event.y)])
+    elif  figura_nova[0] == "linha":
+        figura_nova = ("linha", (figura_nova[1][0], figura_nova[1][1], event.x, event.y))
+    elif figura_nova[0] == "retangulo":
+        figura_nova = ("retangulo", (figura_nova[1][0], figura_nova[1][1], event.x, event.y))
+    elif figura_nova[0] == "circulo":
+        figura_nova = ("circulo", (figura_nova[1][0], figura_nova[1][1], event.x, event.y))
+
+    desenhar_figuras()
+    desenhar_figura_nova()
+
+def incluir_figura_nova(event): 
+    if not incompleta(figura_nova):
+        figuras.append(figura_nova) 
+    desenhar_figuras()
+
+def desenhar_figuras():
+    canvas.delete("all")
+    for fig, values in figuras:
+        if fig == "linha":
+            canvas.create_line(values[0], values[1], values[2], values[3])
+        elif fig == "rabisco":
+            canvas.create_line(values)
+        elif fig == 'retangulo':
+            canvas.create_rectangle(values[0], values[1], values[2], values[3])
+        elif fig == 'circulo':
+             canvas.create_oval(values[0], values[1], values[2], values[3])
+    desenhar_figura_nova()
+def desenhar_figura_nova():
+    fig, values = figura_nova
+    if fig == "linha":
+        canvas.create_line(values[0], values[1], values[2], values[3], dash=(4, 2))
+    elif fig == "rabisco":
+        canvas.create_line(values, dash=(4, 2))
+    elif fig == 'retangulo':
+            canvas.create_rectangle(values[0], values[1], values[2], values[3])
+    elif fig == 'circulo':
+             canvas.create_oval(values[0], values[1], values[2], values[3],dash=(4,2))
+def incompleta(figura):
+    fig, values = figura
+    if fig == "linha":
+        return (values[0], values[1]) == (values[2], values[3])
+    elif fig == "rabisco":
+        return len(values) <= 1
+
+
+
+
+
+figuras = []      
+figura_nova = None 
+
+root = Tk()
+frame = Frame(root)
+
+paddings = {'padx': 5, 'pady': 5} 
+
+label = ttk.Label(frame,  text='Paint')
+label.grid(column=0, row=0, sticky=W, **paddings)
+
+tipo_figura_var = StringVar(root) 
+option_menu = ttk.OptionMenu(frame, tipo_figura_var,
+                             'Linha','Linha', 'Rabisco','retangulo','circulo')
+option_menu.grid(column=1, row=0, sticky=W, **paddings)
+
+canvas = Canvas(frame, bg='white', width=600, height=600)
+canvas.grid(column=0, row=1, columnspan=2, sticky=W, **paddings)
+
+frame.pack()
+
+canvas.bind('<ButtonPress-1>', iniciar_figura_nova)
+canvas.bind('<B1-Motion>', atualizar_figura_nova)
+canvas.bind('<ButtonRelease-1>', incluir_figura_nova)
+
+root.mainloop()
