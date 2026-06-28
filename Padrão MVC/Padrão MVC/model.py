5769c80045f4923fd56abfb6e4f574e08a8a91b9
Mateus <f3yk1br@gmail.com>
2026-06-28 10:43:16 -0300

    Update model.py
    Agora a classe modelo é desinformada de verdade

--- Padrão MVC/Padrão MVC/model.py
+++ Padrão MVC/Padrão MVC/model.py
@@ -1,23 +1,4 @@
-from math import sqrt
-from tkinter import *
-from tkinter import colorchooser
-from tkinter.ttk import *
-from abc import ABC, abstractmethod
-
-class FormasModelo:
-    def __init__(self, canvas, valores, cor_fill, cor_out):
-        self.canvas = canvas
-        self.valores = valores
-        self.cor_fill = cor_fill
-        self.cor_out = cor_out
-
-    @abstractmethod
-    def desenhar(self): pass
-
-    @abstractmethod
-    def desenhar_provisorio(self): pass
-
-class AppModelo:
+class Model:
     def __init__(self):
         self.figuras = []
         self.tipo_atual = "Rabisco"
